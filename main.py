68b94b1c31a52c0aceb50b709ff9b0caf9285e6f
Katsuhisa Yamanaka <yamanaka@192.168.1.25>
2021-02-18 05:43:39 +0900

    Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import opes
+from three_itv_qsort import *
+from four_itv_qsort import *
+from five_itv_qsort import *
+import exp
+
+#
+# Write your program here. After that, perform main.py
+# using python3 command.
+# This program runs on Python 3.8.6
+#
+
+# three_itv_qsort
+#exp.all_exp(7,three_itv_qsort)
+exp.random_exp(47,5,three_itv_qsort)
+
+# four_itv_qsort
+#exp.all_exp(7,four_itv_qsort)
+exp.random_exp(47,5,four_itv_qsort)
+
+# five_itv_qsort
+#exp.all_exp(7,five_itv_qsort)
+exp.random_exp(47,5,five_itv_qsort)
