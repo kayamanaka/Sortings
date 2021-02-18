260cbf9fe2915da840424f781698d2c36609598f
Katsuhisa Yamanaka <yamanaka@MacBook-Air.local>
2021-02-18 13:38:13 +0900

    main.py is modified.

--- main.py
+++ main.py
@@ -10,14 +10,68 @@ import exp
 # This program runs on Python 3.8.6
 #
 
-# three_itv_qsort
-#exp.all_exp(7,three_itv_qsort)
+#
+# three_itv_qsort:
+#
+
+#
+# ``exp.random_exp(47,5,three_itv_qsort)'' returns
+# the average number of flips when we run
+# three-interval quicksort
+# for 5 random permutations of n=47 elements.
+#
 exp.random_exp(47,5,three_itv_qsort)
+#
 
+#
+# ``exp.all_exp(7,three_itv_qsort)'' returns
+# the average number of flips when we run
+# three-interval quicksort
+# for all the permutations of n=7 elements.
+#
+# exp.all_exp(7,three_itv_qsort)
+#
+
+#
 # four_itv_qsort
-#exp.all_exp(7,four_itv_qsort)
-exp.random_exp(47,5,four_itv_qsort)
+#
 
+#
+# ``exp.random_exp(47,5,four_itv_qsort)'' returns
+# the average number of flips when we run
+# four-interval quicksort
+# for 5 random permutations of n=47 elements.
+#
+#exp.random_exp(47,5,four_itv_qsort)
+#
+
+#
+# ``exp.all_exp(7,four_itv_qsort)'' returns
+# the average number of flips when we run
+# four-interval quicksort
+# for all the permutations of n=7 elements.
+#
+#exp.all_exp(7,four_itv_qsort)
+#
+
+#
 # five_itv_qsort
+#
+
+#
+# ``exp.random_exp(47,5,five_itv_qsort)'' returns
+# the average number of flips when we run
+# five-interval quicksort
+# for 5 random permutations of n=47 elements.
+#
+#exp.random_exp(47,5,five_itv_qsort)
+#
+
+#
+# ``exp.all_exp(7,five_itv_qsort)'' returns
+# the average number of flips when we run
+# five-interval quicksort
+# for all the permutations of n=7 elements.
+#
 #exp.all_exp(7,five_itv_qsort)
-exp.random_exp(47,5,five_itv_qsort)
+#
